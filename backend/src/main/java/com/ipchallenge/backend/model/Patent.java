f48ef8fb9f5c3ff74b3cf463804c532fd7d036ac
as-bowley <as.bowley@gmail.com>
2022-11-16 17:49:32 +0100

    update schema for patent and fix details for connecting to mysql database

--- backend/src/main/java/com/ipchallenge/backend/model/Patent.java
+++ backend/src/main/java/com/ipchallenge/backend/model/Patent.java
@@ -3,13 +3,14 @@ package com.ipchallenge.backend.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.sql.Date;
 
 @Entity
 public class Patent {
     @Id
     @GeneratedValue
     private int publication_id;
-    private int publication_date;
+    private String publication_date;
     private String title;
 
     public int getPublication_id() {
@@ -20,11 +21,11 @@ public class Patent {
         this.publication_id = publication_id;
     }
 
-    public int getPublication_date() {
+    public String getPublication_date() {
         return publication_date;
     }
 
-    public void setPublication_date(int publication_date) {
+    public void setPublication_date(String publication_date) {
         this.publication_date = publication_date;
     }
 
